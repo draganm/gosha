66244beb124ba68400038a3db118cdd4a9db7ad5
Dragan Milic <dragan@netice9.com>
2023-10-30 11:19:21 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/draganm/gosha
+
+go 1.20
+
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/urfave/cli/v2 v2.25.7
+	golang.org/x/tools v0.14.0
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/samber/lo v1.38.1 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+	golang.org/x/mod v0.13.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+
+	"github.com/samber/lo"
+	"github.com/urfave/cli/v2"
+	"golang.org/x/exp/slices"
+	"golang.org/x/tools/go/packages"
+)
+
+func main() {
+
+	app := &cli.App{
+		Flags: []cli.Flag{},
+		Action: func(c *cli.Context) error {
+
+			packagePath := c.Args().First()
+
+			if packagePath == "" {
+				packagePath = "."
+			}
+
+			cfg := &packages.Config{
+				Mode: packages.NeedDeps |
+					packages.NeedImports |
+					packages.NeedName |
+					packages.NeedEmbedFiles |
+					packages.NeedFiles,
+				Dir: packagePath,
+			}
+			pkgs, err := packages.Load(cfg, ".")
+			if err != nil {
+				return fmt.Errorf("could not open packages: %w", err)
+			}
+
+			if packages.PrintErrors(pkgs) > 0 {
+				return errors.New("packages.Load returned errors")
+			}
+
+			allPackages := map[string]*packages.Package{}
+
+			for _, pkg := range pkgs {
+				visitDeps(pkg, allPackages)
+			}
+
+			packageNames := lo.Keys(allPackages)
+			slices.Sort(packageNames)
+
+			sum := sha256.New()
+
+			for _, packageName := range packageNames {
+				sum.Write([]byte(packageName))
+				pkg := allPackages[packageName]
+				err = sortAndCopyFiles(sum, pkg.GoFiles)
+				if err != nil {
+					return fmt.Errorf("could not add go files: %w", err)
+				}
+				err = sortAndCopyFiles(sum, pkg.EmbedFiles)
+				if err != nil {
+					return fmt.Errorf("could not add embedded files: %w", err)
+				}
+				err = sortAndCopyFiles(sum, pkg.OtherFiles)
+				if err != nil {
+					return fmt.Errorf("could not add other files: %w", err)
+				}
+			}
+
+			finalSha := sum.Sum(nil)
+
+			fmt.Println(hex.EncodeToString(finalSha))
+
+			return nil
+		},
+	}
+
+	app.RunAndExitOnError()
+
+}
+
+func sortAndCopyFiles(destination io.Writer, files []string) error {
+	sort.Strings(files)
+	for _, f := range files {
+		err := copyFile(destination, f)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func copyFile(destination io.Writer, fileName string) error {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return fmt.Errorf("could not open %s :%w", fileName, err)
+	}
+
+	defer f.Close()
+	_, err = io.Copy(destination, f)
+	if err != nil {
+		return fmt.Errorf("failed to copy %s: %w", err)
+	}
+
+	return nil
+}
+
+func visitDeps(pkg *packages.Package, visit map[string]*packages.Package) {
+	_, alreadyVisited := visit[pkg.PkgPath]
+	if alreadyVisited {
+		return
+	}
+	visit[pkg.PkgPath] = pkg
+
+	for _, dep := range pkg.Imports {
+		visitDeps(dep, visit)
+	}
+
+}
